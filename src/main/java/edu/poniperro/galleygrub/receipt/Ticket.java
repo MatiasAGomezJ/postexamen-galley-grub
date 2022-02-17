45c37b044f3378a16278f6b86657d06ffbaef54b
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-02-17 20:10:11 +0100

    feat(Receipt): Añadidos metodos de la interfaz

--- src/main/java/edu/poniperro/galleygrub/receipt/Ticket.java
+++ src/main/java/edu/poniperro/galleygrub/receipt/Ticket.java
@@ -1,9 +1,14 @@
 package edu.poniperro.galleygrub.receipt;
 
+import edu.poniperro.galleygrub.extras.Extra;
 import edu.poniperro.galleygrub.order.Comanda;
 
 public interface Ticket {
     Comanda getOrder();
+    void setChain(Extra extra);
+    Extra getChain();
     Double total();
+    void sumExtraCharge();
     void print();
+
 }
