83b9fa61b9a7c2d73c63309939323e06e56560e3
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-02-17 17:30:07 +0100

    feat(Product): Añadida interfaz Product

---
+++ src/main/java/edu/poniperro/galleygrub/items/Product.java
@@ -0,0 +1,8 @@
+package edu.poniperro.galleygrub.items;
+
+public interface Product {
+    public String name();
+    public Double price();
+    public String extra();
+    public Boolean isRegular();
+}
