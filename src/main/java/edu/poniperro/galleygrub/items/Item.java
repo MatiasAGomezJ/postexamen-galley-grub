58054b10ef0970390995367a95d122282f5fd56f
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-02-17 17:30:22 +0100

    feat(Item): Añadida clase Item con metodos

---
+++ src/main/java/edu/poniperro/galleygrub/items/Item.java
@@ -0,0 +1,42 @@
+package edu.poniperro.galleygrub.items;
+
+public class Item implements Product{
+    String name;
+    Double price;
+    String extra;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Item(String name, double price, String extra) {
+        this.name = name;
+        this.price = price;
+        this.extra = extra;
+    }
+    @Override
+    public String name() {
+        return this.name;
+    }
+
+    @Override
+    public Double price() {
+        return this.price;
+    }
+
+    @Override
+    public String extra() {
+        return this.extra;
+    }
+
+    @Override
+    public Boolean isRegular() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return name() + ", " + price();
+    }
+}
