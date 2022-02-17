3bd0e776c07f7e2f8422e03db86d68bfd39911c8
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-02-17 20:23:06 +0100

    feat: Añadidas varias clases hijas de Extra

---
+++ src/main/java/edu/poniperro/galleygrub/extras/CheeseExtra.java
@@ -0,0 +1,11 @@
+package edu.poniperro.galleygrub.extras;
+
+import edu.poniperro.galleygrub.order.Comanda;
+
+public class CheeseExtra extends Extra {
+
+    @Override
+    public void sumExtras(Comanda order) {
+
+    }
+}
