a0b0b39359afe8d7cc606ae61044bfee0c1baa5a
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-02-17 21:05:44 +0100

    feat(CheeseExtra): sumExtra

--- src/main/java/edu/poniperro/galleygrub/extras/CheeseExtra.java
+++ src/main/java/edu/poniperro/galleygrub/extras/CheeseExtra.java
@@ -1,11 +1,17 @@
 package edu.poniperro.galleygrub.extras;
 
+import edu.poniperro.galleygrub.items.Item;
+import edu.poniperro.galleygrub.items.Prices;
 import edu.poniperro.galleygrub.order.Comanda;
 
 public class CheeseExtra extends Extra {
 
     @Override
     public void sumExtras(Comanda order) {
-
+        for (Item item : order.itemList()) {
+            if (!item.isRegular() && item.extra() == "cheese") {
+                order.updateTotal(Prices.getPriceof(item.extra()));
+            }
+        }
     }
 }
