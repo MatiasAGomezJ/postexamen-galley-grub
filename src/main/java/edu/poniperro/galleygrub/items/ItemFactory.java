23333cc4979fb0e66493f65e047e5b4d278bc994
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-03-24 00:23:25 +0100

    feat(itemfactory): Añadir clase con sus métodos

--- src/main/java/edu/poniperro/galleygrub/items/ItemFactory.java
+++ src/main/java/edu/poniperro/galleygrub/items/ItemFactory.java
@@ -1,5 +1,34 @@
 package edu.poniperro.galleygrub.items;
 
+import java.util.HashMap;
+
 public class ItemFactory {
 
+    private static HashMap<String, Item> itemMap;
+
+    public ItemFactory() {
+        itemMap = new HashMap<>();
+    }
+
+    public static Item getItem(String name, double price) {
+         if (itemMap.containsKey(name)){
+             return itemMap.get(name);
+         }
+         return new Item(name, price);
+    }
+
+    public static Item getItem(String name, double price, String extra) {
+        if (itemMap.containsKey(name)){
+            return itemMap.get(name);
+        }
+        return new Item(name, price, extra);
+    }
+
+    public static int size() {
+        return itemMap.size();
+    }
+
+    public static void clear() {
+        itemMap.clear();
+    }
 }
