5c6672d057af9b04267c7b06168f1dec380f717e
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-03-24 01:43:32 +0100

    fix(itemfactory): Comprobar correctamente si ya estaba creado el item

--- src/main/java/edu/poniperro/galleygrub/items/ItemFactory.java
+++ src/main/java/edu/poniperro/galleygrub/items/ItemFactory.java
@@ -4,24 +4,34 @@ import java.util.HashMap;
 
 public class ItemFactory {
 
-    private static HashMap<String, Item> itemMap;
+    private static HashMap<String, Item> itemMap = new HashMap<>();
 
-    public ItemFactory() {
-        itemMap = new HashMap<>();
-    }
+//    !itemMap.isEmpty() &&
 
     public static Item getItem(String name, double price) {
-         if (itemMap.containsKey(name)){
-             return itemMap.get(name);
-         }
-         return new Item(name, price);
+        Item item;
+        if (itemMap.containsKey(name)){
+            item = itemMap.get(name);
+            if(item.name() == name && item.price() == price) {
+                return itemMap.get(name);
+            }
+        }
+        item = new Item(name, price);
+        itemMap.put(name, item);
+        return item;
     }
 
     public static Item getItem(String name, double price, String extra) {
+        Item item;
         if (itemMap.containsKey(name)){
-            return itemMap.get(name);
+            item = itemMap.get(name);
+            if(item.name() == name && item.price() == price && item.extra() == extra) {
+                return itemMap.get(name);
+            }
         }
-        return new Item(name, price, extra);
+        item = new Item(name, price, extra);
+        itemMap.put(name, item);
+        return item;
     }
 
     public static int size() {
